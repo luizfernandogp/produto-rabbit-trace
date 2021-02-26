333bbed9a679cc950d66d3bdb8ff405e1deb337b
luiz fernando <luiz.pinto@grupopan.com>
2021-02-26 10:28:34 -0300

    remove test context and update url jaeger local

--- src/test/java/com/pocms/produtos/ProdutoApplicationTests.java
+++ src/test/java/com/pocms/produtos/ProdutoApplicationTests.java
@@ -3,7 +3,6 @@ package com.pocms.produtos;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ProdutoApplicationTests {
 
 	@Test
